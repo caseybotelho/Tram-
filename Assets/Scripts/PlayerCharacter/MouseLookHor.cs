1470fda9061c96dace7a1bb13f0132c007ddcf1e
caseybotelho <caseybotelho@gmail.com>
2017-02-16 14:04:43 -0500

    Initial commit
    All current assets.

---
+++ Assets/Scripts/PlayerCharacter/MouseLookHor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLookHor : MonoBehaviour {
+
+	public float sens = 9.0f;
+
+	void Start () {
+		Rigidbody body = GetComponent<Rigidbody>();
+		if (body) {
+			body.freezeRotation = true;
+		}
+	}
+
+	void Update () {
+		transform.Rotate (0, Input.GetAxis ("Mouse X") * sens, 0);
+	}
+}
---
+++ Assets/Scripts/PlayerCharacter/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Controls/FPS Movement")]
+public class Movement : MonoBehaviour {
+
+	public float speed = 6.0f;
+	float gravity = -9.8f;
+
+	private CharacterController player;
+
+	void Start () {
+		player = GetComponent<CharacterController> ();
+	}
+
+	void Update () {
+		float movX = Input.GetAxis ("Horizontal") * speed;
+		float movZ = Input.GetAxis ("Vertical") * speed;
+
+		Vector3 mov = new Vector3 (movX, 0, movZ);
+		mov = Vector3.ClampMagnitude (mov, speed);
+		mov.y = gravity;
+		mov *= Time.deltaTime;
+		mov = transform.TransformDirection (mov);
+
+		player.Move (mov);
+	}
+}
